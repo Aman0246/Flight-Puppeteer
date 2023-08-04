4626b4f3736d8be9ef62ba22d777d3058bcce87c
Aman0246 <amankashyap0246@gmail.com>
2023-08-04 11:24:37 +0530

    backend Done

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+const { routes } = require("./Routes/routes")
+const express=require('express')
+require('dotenv').config()
+const app =express()
+
+app.use(express.json())
+app.use('/',routes)
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
