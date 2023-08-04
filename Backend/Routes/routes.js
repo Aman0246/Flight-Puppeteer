476342308ad0d9777e86b7b3bf82fe41ea210f59
Aman0246 <amankashyap0246@gmail.com>
2023-08-04 16:49:01 +0530

    frontfileMade

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -2,6 +2,6 @@ const express=require('express')
 const { scrappigData } = require('../Controller/flight')
 const routes=express.Router()
 
-routes.post('/scrappigData',scrappigData)
+routes.get('/scrappigData',scrappigData)
 
 module.exports={routes}
